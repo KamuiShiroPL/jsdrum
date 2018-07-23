c04610cf9603d886ad215f4e97fa60d851830e53
Pawel Dudek <paweldudek1988@gmail.com>
2018-07-23 12:55:10 +0900

    Modify audio reverse for event listener

--- drum.js
+++ drum.js
@@ -1,8 +1,12 @@
 window.addEventListener('keydown', function (e) {
     // console.log(e.keyCode);
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; // stop the function
     console.log(audio);
+    audio.currentTime = 0; // rewind audio file to start
+    audio.play();
+    console.log(key);
 });
 
 
