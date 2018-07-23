d4e7d1427bf8d22da485809914cd53ee9f5c6d35
Pawel Dudek <paweldudek1988@gmail.com>
2018-07-23 12:47:07 +0900

    Modify JS , bind key with audio file by event listener

---
+++ drum.js
@@ -0,0 +1,8 @@
+window.addEventListener('keydown', function (e) {
+    // console.log(e.keyCode);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    
+    console.log(audio);
+});
+
+
