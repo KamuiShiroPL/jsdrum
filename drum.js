e23ac8d1c48743cc8607d79589d61d0fcf1bc8ff
Pawel Dudek <paweldudek1988@gmail.com>
2018-07-24 13:54:49 +0900

    Add remove transition function - remove css playing class after transition end

--- drum.js
+++ drum.js
@@ -1,12 +1,23 @@
-window.addEventListener('keydown', function (e) {
-    // console.log(e.keyCode);
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!audio) return; // stop the function
-    console.log(audio);
-    audio.currentTime = 0; // rewind audio file to start
-    audio.play();
-    console.log(key);
-});
 
+function playSound (e) {
+        // console.log(e.keyCode);
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+        if(!audio) return; // stop the function
+        // console.log(audio);
+        audio.currentTime = 0; // rewind audio file to start
+        audio.play();
+        // console.log(key);
+        key.classList.add('playing');
+}
 
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;
+     // skip it if it's not transform
+    console.log(e.propertyName);
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
